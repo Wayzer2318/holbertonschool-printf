f7e762687b3d65d18b031f913fcab329d7ff3f1d
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-10 18:34:25 +0100

    task0

--- _printf.c
+++ _printf.c
@@ -1,2 +1,18 @@
 #include "main.h"
-#include "stdio."
+#include <stdio.h>
+#include <stdarg.h>
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int len;
+
+	print_t prnt[] = {
+		{"c", _putchar},
+		{"s", puts},
+		{"%", printpour},
+		{NULL,NULL}
+	};
+
+	if (format == NULL)
+		return(-1);
+}

e7a52b5a55549cefead8a25bf8514d4d853d3ab2
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-13 23:40:19 +0100

    modifing the main.h file

--- main.h
+++ main.h
@@ -4,12 +4,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-typedef struct print
-{
-	char *data;
-	int (*fun)(va_list arg);
-} print_t;
-
 int _printf(const char *format, ...);
 void _puts(char *str);
 int _putchar(char c);
